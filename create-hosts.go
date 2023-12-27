68a75abf8e5cbd325bcfb4dc7a78f75f04cd0dbd
Luis Contreras <luis.contreras.do@gmail.com>
2023-12-26 21:29:12 -0400

    Update create-hosts.go

--- create-hosts.go
+++ create-hosts.go
@@ -1,7 +1,7 @@
 // This script in golang will perform the following tasks
 // Create a file named all_hosts.cfg with 100 hosts with the ping service check, the secuence of the names will be host###
 // It will create an entry to the nagios.cfg pointing the file cfg_file=/usr/local/nagios/etc/objects/all_hosts.cfg
-// Asignar el permiso de usuario y grupo al archivo  all_hosts.cf
+// Assign user and group permission to the file all_hosts.cfg
 // Nagios service will be restarted
 
 package main
